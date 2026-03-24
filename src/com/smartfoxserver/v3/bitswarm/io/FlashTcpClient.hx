58b5add6ce24da97499e903460835297f8568b6c
barisyild <fb_bcy@hotmail.com>
2026-03-24 05:32:33 +0300

    Add `openfl` support to FlashTcpClient and FlashUdpClient platform checks

--- src/com/smartfoxserver/v3/bitswarm/io/FlashTcpClient.hx
+++ src/com/smartfoxserver/v3/bitswarm/io/FlashTcpClient.hx
@@ -1,6 +1,6 @@
 package com.smartfoxserver.v3.bitswarm.io;
 
-#if flash
+#if (flash || openfl)
 import com.smartfoxserver.v3.ConfigData;
 import com.smartfoxserver.v3.bitswarm.BitSwarmClient;
 import com.smartfoxserver.v3.bitswarm.SocketState;
--- src/com/smartfoxserver/v3/bitswarm/io/FlashUdpClient.hx
+++ src/com/smartfoxserver/v3/bitswarm/io/FlashUdpClient.hx
@@ -1,6 +1,6 @@
 package com.smartfoxserver.v3.bitswarm.io;
 
-#if flash
+#if (flash || openfl)
 import com.smartfoxserver.v3.bitswarm.BitSwarmClient;
 import com.smartfoxserver.v3.bitswarm.BitSwarmEvent;
 import com.smartfoxserver.v3.bitswarm.SocketState;
